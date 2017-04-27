fb2c9c19f0b4b4015895341cb5ca049de1a76b69
André Abadesso <andre.abadesso@gmail.com>
2017-04-26 23:25:06 -0300

    feat: added state to infrared

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const restify       = require('restify'),
       config        = require('./lib/config');
 
 const channelsState = require('./lib/ChannelsState');
+const infraredState = require('./lib/InfraredState');
  
 let server = restify.createServer({
   name: 'Hubot',
@@ -39,31 +40,30 @@ server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
-let ws = new WebSocket(config.ws, {
-    perMessageDeflate: false
-});
+let ws = undefined;
 
-/*ws.on('message', (message, flags) => {
-    let data = (JSON.parse(message));
+function connectWS() {
+    try {
+        let ws = new WebSocket(config.ws, {
+            perMessageDeflate: false
+        });
 
-    if (data.message_type && data.message_type === 'channel_update') {
-        channelsState.addState(data); 
+        ws.on('message', (message, flags) => {
+            let data = JSON.parse(message);
+
+            if (data.message_type && data.message_type === 'channel_update') {
+                channelsState.addState(data);
+            }
+
+            if (data.message_type && data.message_type === 'infrared_update') {
+                infraredState.addState(data);
+            }
+        });
+    } catch(e) {
+        setTimeout(() => {
+            console.log('Retrying WS connection..');
+            connectWS();
+        }, 5000);
     }
 });
-*/
-/*
-swagger.init(server, {
-    apiVersion: '1.0',
-    swaggerVersion: '1.0',
-    basePath: 'http://localhost:8080',
-    info: {
-        title: 'Hubot Home API',
-        description: 'Hubot Swagger'
-    },
-    apis: ['./lib/routes/index.js'],
-    swaggerURL: '/swagger',
-    swaggerJSON: '/api-docs.json',
-    swaggerUI: './public'
-});
-*/
 
