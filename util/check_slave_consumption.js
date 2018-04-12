b5a0d75bb18e82ffdbb72914013678c3911135aa
André Abadesso <andre.abadesso@gmail.com>
2018-04-12 03:11:50 -0300

    feat: added script to check consumption

---
+++ util/check_slave_consumption.js
@@ -0,0 +1,37 @@
+let models = require('./lib/models')
+let Promise = require('bluebird')
+let _ = require('lodash')
+
+models.Slave.findAll()
+  .then((slaves) => {
+    let ids = _.map(slaves, 'id')
+
+    return Promise.all(slaves.map((slave) => { 
+      return models.RawEnergy.findAll({
+        where: {
+          'slave_id': {
+            $in: ids
+          }
+        },
+        order: [
+          'datetime',
+          'DESC'
+        ],
+        limit: 1
+      }).then((consumption) => {
+          return [slave, _.first(consumption)]
+      })
+    })).then((result) => {
+      return _.map(result, (data) => {
+        if (data[1]) {
+          let regularConsumption = data[0].regular_consumption
+          if (data[1].value > 0 && data[1].value < data[0].regular_consumption) {
+            console.log('ALERT !!!!')
+          } else {
+            console.log('REGULAR !!!')
+            console.log(data[1].value, data[0].regular_consumption)
+          }
+        }
+      })
+    })
+  })
