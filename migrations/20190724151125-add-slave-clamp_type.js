dd59f329d6928e64754d9fcd6826288ae2f6e5b9
André Abadesso <andre.abadesso@gmail.com>
2019-07-24 14:41:42 -0300

    feat(): added clamp_type to slave

---
+++ migrations/20190724151125-add-slave-clamp_type.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn(
+            'slaves',
+            'clamp_type',
+            type: Sequelize.INTEGER
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.removeColumn('slaves', 'clamp_type')
+    }
+};
