470f5d6207b856318e03366ddfecf1adda5d25db
André Abadesso <andre.abadesso@gmail.com>
2019-07-31 19:49:17 -0300

    fix(api): fixed timescale consumption migration

--- migrations/20190731175027-energy_timescale.js
+++ migrations/20190731175027-energy_timescale.js
@@ -2,10 +2,27 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
+        queryInterface.sequelize.connectionManager.config.database = process.env.DB_DATABASE
+        queryInterface.sequelize.connectionManager.config.username = 'root'
+        queryInterface.sequelize.connectionManager.config.password = process.env.DB_PASSWORD
+        queryInterface.sequelize.connectionManager.config.host = process.env.HOST
+        queryInterface.sequelize.connectionManager.pool.clear()
+
         let migrate = `
         BEGIN;
-            ALTER TABLE consumption_realtime DROP column id;
+            ALTER TABLE consumption_realtime RENAME TO consumption_realtime_old;
+
+            CREATE TABLE public.consumption_realtime (
+                "timestamp" timestamp(0) with time zone NOT NULL DEFAULT now(),
+                slave_id integer NOT NULL,
+                ambient_id integer[],
+                value real NOT NULL
+            );
+
             SELECT create_hypertable('consumption_realtime', 'timestamp');
+
+            INSERT INTO consumption_realtime SELECT timestamp, slave_id, ambient_id, value FROM consumption_realtime_old;
+
         COMMIT;
         `
         return queryInterface.sequelize.query(migrate);
