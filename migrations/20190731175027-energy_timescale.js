bd266a3500e973917f6f3a1b844c2d2fe91dae94
André Abadesso <andre.abadesso@gmail.com>
2019-07-31 21:13:40 -0300

    feat(api): added create extension and drop table consumption_realtime_old to api

--- migrations/20190731175027-energy_timescale.js
+++ migrations/20190731175027-energy_timescale.js
@@ -4,6 +4,8 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         let migrate = `
         BEGIN;
+			CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
+
             ALTER TABLE consumption_realtime RENAME TO consumption_realtime_old;
 
             CREATE TABLE public.consumption_realtime (
@@ -16,6 +18,8 @@ module.exports = {
             SELECT create_hypertable('consumption_realtime', 'timestamp');
 
             INSERT INTO consumption_realtime SELECT timestamp, slave_id, ambient_id, value FROM consumption_realtime_old;
+
+			DROP TABLE consumption_realtime_old;
 
         COMMIT;
         `
