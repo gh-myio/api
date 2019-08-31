7df6b3cc89eb23285bfe6fc233b14c75299b528f
Yan Minari <yangm97@gmail.com>
2019-08-31 16:13:43 -0300

    chore(api): fix standard warnings
    Signed-off-by: Yan Minari <yangm97@gmail.com>

--- migrations/20190831155633-add-slave-aggregate.js
+++ migrations/20190831155633-add-slave-aggregate.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.addColumn(
-            'slaves',
-            'aggregate', {
-                type: Sequelize.BOOLEAN,
-                defaultValue: true
-            }
-        );
-    },
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'slaves',
+      'aggregate', {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      }
+    )
+  },
 
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.removeColumn('slaves', 'aggregate')
-    }
-};
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('slaves', 'aggregate')
+  }
+}
