ac5d1b4382f840fb64c3cc686f3581f7b514ccc1
André Abadesso <andre.abadesso@gmail.com>
2020-05-28 07:12:20 -0300

    fix(): alert_history missing attributes and name
    AlertHistory was missing Thu 28 May 2020 07:12:20 -03 attribute and sequelize was messing with table name trying to make it plural. Added freezeTableName parameter to fix it

---
+++ migrations/20200528100514-add-alert_history-date.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'alert_history',
+      'date', {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'alert_history',
+      'date'
+    )
+  }
+};
