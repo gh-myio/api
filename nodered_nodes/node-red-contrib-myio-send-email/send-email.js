a506daaee0c7dbce0cdd93cb4fa059a56babc10f
André Abadesso <andre.abadesso@gmail.com>
2020-08-31 17:46:32 -0300

    feat(api): added send-email node

---
+++ nodered_nodes/node-red-contrib-myio-send-email/send-email.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken')
+const request = require('request')
+
+module.exports = function (RED) {
+  function SendEmail (config) {
+    RED.nodes.createNode(this, config)
+
+    const node = this
+
+    node.on('input', function (msg) {
+      const centralUUID = global.centralUUID
+      const token = jwt.sign({
+        UUID: global.centralUUID
+      }, global.privKey)
+
+      const headers = {
+        'x-access-token': token,
+        'central_id': centralUUID
+      }
+
+      request.post('https://server.myio.com.br/central/email', {
+        json: {
+          to: msg.payload.to,
+          title: msg.payload.title,
+          template: msg.payload.template
+        },
+        headers
+      }, (error, res, body) => {
+        if (error) {
+          console.error(error)
+
+          return
+        }
+
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+      })
+    })
+  }
+
+  RED.nodes.registerType('send-email', SendEmail)
+}
