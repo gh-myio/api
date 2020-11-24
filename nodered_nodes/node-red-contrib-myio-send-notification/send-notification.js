d29a9145c55ba6ff35acb95d569d37e3a2c08135
André Abadesso <andre.abadesso@gmail.com>
2020-11-24 14:17:02 -0300

    feat(api): added send-notification node to node-red

---
+++ nodered_nodes/node-red-contrib-myio-send-notification/send-notification.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken')
+const request = require('request')
+
+module.exports = function (RED) {
+  function SendNotification (config) {
+    RED.nodes.createNode(this, config)
+
+    const node = this
+
+    node.on('input', function (msg) {
+      const centralUUID = global.centralUUID
+      const token = jwt.sign({
+        UUID: global.centralUUID
+      }, global.privKey)
+
+      const headers = {
+        'x-access-token': token,
+        central_id: centralUUID
+      }
+
+      request.post('https://server.myio.com.br/central/custom_push', {
+        json: {
+          UUID: centralUUID,
+          token: token,
+          title: msg.payload.title,
+          subtitle: msg.payload.subtitle,
+          users: msg.payload.users
+        },
+        headers
+      }, (error, res, body) => {
+        if (error) {
+          console.error(error)
+
+          return
+        }
+
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+      })
+    })
+  }
+
+  RED.nodes.registerType('send-notification', SendNotification)
+}
