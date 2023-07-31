20d844f6ed6a5bdacf4b64087380d33e6adaeff5
Yan Minari <yangm97@gmail.com>
2023-07-31 17:37:54 -0300

    feat(red): add rf/ir transmit node

---
+++ nodered_nodes/node-red-contrib-myio-transmit-rfir-command/transmit-rfir-command.js
@@ -0,0 +1,51 @@
+const models = require('../../lib/models').Models
+const WebSocketHandler = require('../../lib/WebSocketHandler')
+
+module.exports = function (RED) {
+  function TransmitRfirCommand (config) {
+    RED.nodes.createNode(this, config)
+
+    const node = this
+
+    node.on('input', function (msg) {
+      const slaveId = RED.util.evaluateNodeProperty(config.slave, config.slaveType, node, msg)
+      const rfirCommandId = RED.util.evaluateNodeProperty(config.command, config.commandType, node, msg)
+      const wsPayload = {
+        type: 'slave',
+        id: parseInt(slaveId),
+        command: 'transmit',
+        rfir_command_id: parseInt(rfirCommandId)
+      }
+      WebSocketHandler.send(JSON.stringify(wsPayload))
+    })
+  }
+
+  RED.nodes.registerType('transmit-rfir-command', TransmitRfirCommand)
+
+  RED.httpAdmin.get('/v0/rfir_commands', async (req, res) => {
+    const slaves = await models.Slave.findAll({
+      include: [{
+        as: 'devices',
+        model: models.RfirDevices,
+        required: true,
+        include: [{
+          // as: 'remotes',
+          model: models.RfirRemotes,
+          required: true,
+          include: [{
+            // as: 'buttons',
+            model: models.RfirButtons,
+            required: true,
+            include: [{
+              // as: 'command',
+              required: true,
+              model: models.RfirCommands
+            }]
+          }]
+        }]
+      }]
+    })
+
+    res.send(slaves)
+  })
+}
