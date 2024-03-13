e3556e3f0f6c10cd4e18d9a2e4ce30be1ae57064
André Abadesso <andre.abadesso@gmail.com>
2024-03-13 17:15:37 -0300

    fix: checking if time is < 2023 and retrying until time updated

--- scheduler.js
+++ scheduler.js
@@ -1,13 +1,17 @@
 const scheduler = require('node-schedule')
+
 let schedules = []
 let ws
+let scheduleTimer = null;
 
 module.exports = {
   setSocket: (socket) => { ws = socket },
   setupEnergy: () => {},
-  prepare: () => {
+  prepare: function() {
     const _ = require('lodash')
     const models = require('./lib/models').Models
+
+    clearTimeout(scheduleTimer);
 
     if (schedules.length > 0) {
       schedules.forEach((schedule) => {
@@ -15,6 +19,14 @@ module.exports = {
 
         schedule.cancel()
       })
+    }
+
+    const now = new Date().getTime();
+
+    if (now < 1701302400000) {
+      console.error('Time is in the past, trying to create schedules again in 5 seconds..')
+      scheduleTimer = setTimeout(() => this.prepare(), 5000);
+      return;
     }
 
     models.Schedule.findAll()
